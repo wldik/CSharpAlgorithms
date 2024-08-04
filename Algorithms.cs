3cbb05442ee445fd4fda456bbd8d586dd0232588
wladik <vladyslavatur@gmail.com>
2024-08-04 10:12:57 +0300

    Insertion Sort

---
+++ Algorithms.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Algorithms
+{
+    public static class Sorting
+    {
+        public static void InsertionSort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 1; i < length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ Testing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 8, 8, 4, 0, 2, 12, 6, 2, 89, 434, 97, 67 };
+            Console.WriteLine(string.Join(", ", array));
+            Sorting.InsertionSort(array);
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
