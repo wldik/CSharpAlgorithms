11cb41d55006ad869ddb35383146c976242e5948
wladik <vladyslavatur@gmail.com>
2024-08-05 10:45:12 +0300

    Insertion Sort. Ascending and Descending oreder added

--- Algorithms.cs
+++ Algorithms.cs
@@ -3,7 +3,7 @@ namespace Algorithms
 {
     public static class Sorting
     {
-        public static void InsertionSort(int[] arr)
+        public static void InsertionSortAscend(int[] arr)
         {
             int length = arr.Length;
             for (int i = 1; i < length; i++)
@@ -11,6 +11,22 @@ namespace Algorithms
                 int key = arr[i];
                 int j = i - 1;
                 while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
+        public static void InsertionSortDesc(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 1; i < length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] < key)
                 {
                     arr[j + 1] = arr[j];
                     j--;
--- Testing.cs
+++ Testing.cs
@@ -2,13 +2,15 @@
 
 namespace Algorithms
 {
-    class Program
+    class Testing
     {
         static void Main(string[] args)
         {
             int[] array = { 2, 8, 8, 4, 0, 2, 12, 6, 2, 89, 434, 97, 67 };
             Console.WriteLine(string.Join(", ", array));
-            Sorting.InsertionSort(array);
+            Sorting.InsertionSortAscend(array);
+            Console.WriteLine(string.Join(", ", array));
+            Sorting.InsertionSortDesc(array);
             Console.WriteLine(string.Join(", ", array));
         }
     }
